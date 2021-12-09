53a0d41bc93e091cf35cb7436c1f7d86d157fbb3
jimmyx1428 <90877135+jimmyx1428@users.noreply.github.com>
2021-12-09 15:53:23 -0500

    cambios para crear producto

--- FrontendEquiposTecnologicos/src/app/modulos/administracion/productos/crear-producto/crear-producto.component.ts
+++ FrontendEquiposTecnologicos/src/app/modulos/administracion/productos/crear-producto/crear-producto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModeloIdentificar } from 'src/app/Modelos/identificar.modelo';
+import { ModeloProducto } from 'src/app/Modelos/producto.modelo';
+import { ProductoService } from 'src/app/servicios/producto.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -8,12 +11,44 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CrearProductoComponent implements OnInit {
 
-  //fgValidador: FormGroup = this.fb.group({
+  fgValidador : FormGroup = this.fb.group({
+    'tipo': ["",[Validators.required]],
+    'descripcion': ["",[Validators.required]],
+    'valor': ["",[Validators.required]],
+    'cantidad': ["",[Validators.required]],
+    'imagen': ["",[Validators.required]]
+  });
 
-  //})
-  constructor(/*private fb:FormBuilder*/) { }
-
+  constructor(private fb:FormBuilder,
+    private servicioProducto:ProductoService) { }
+    
   ngOnInit(): void {
   }
+
+  GuardarProducto(){
+    let tipo = this.fgValidador.controls['tipo'].value;
+    let descripcion = this.fgValidador.controls['descripcion'].value;
+    let valor = parseInt(this.fgValidador.controls['valor'].value);
+    let cantidad = parseInt(this.fgValidador.controls['cantidad'].value);
+    let imagen = this.fgValidador.controls['imagen'].value;
+  
+    let p = new ModeloProducto();
+    p.tipo=tipo;
+    p.descripcion =descripcion;
+    p.valor = valor;
+    p.cantidad = cantidad;
+    p.imagen = imagen;
+
+    this.servicioProducto.CrearProducto(p).subscribe((datos:ModeloProducto)=>{
+      alert("El producto fue creado correctamente!!")
+    },(error:any)=>{
+      alert("Error en el alamcenamiento del producto!!")
+    }
+    )
+  
+  }
+
+
+
 
 }
